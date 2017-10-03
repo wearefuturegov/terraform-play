cfe43a8bbaf04af1f439baafd15e33b57cfae943
Jason Rae <jrae@wearefuturegov.com>
2017-10-03 16:08:05 +0100

    Playing around with route53.. Will come back later

--- route53.tf
+++ route53.tf
@@ -1,13 +1,14 @@
-#resource "aws_route53_zone" "patchwork" {
-#   name = "test.patchowkhq"
-#}
-#resource "aws_route53_record" "server1-record" {
+# resource "aws_route53_zone" "patchwork" {
+#   name = "test.patchworkhq"
+# }
+# resource "aws_route53_record" "server1-record" {
 #   zone_id = "${aws_route53_zone.patchwork.zone_id}"
 #   name = "server1.test.patchowkhq"
 #   type = "A"
 #   ttl = "300"
-#   records = ["104.236.247.8"]
-#}
+#   records = ["${aws_eip.patchwork.public_ip}"]
+# }
+
 #resource "aws_route53_record" "www-record" {
 #   zone_id = "${aws_route53_zone.patchwork.zone_id}"
 #   name = "www.test.patchowkhq"
@@ -32,3 +33,7 @@
 #output "ns-servers" {
 #   value = "${aws_route53_zone.patchwork.name_servers}"
 #}
+
+# output "eip" {
+#   value = "${aws_eip.nat.public_ip}"
+# }
